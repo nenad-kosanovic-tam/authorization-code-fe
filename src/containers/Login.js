7350cd3a576a476bac7ca64a38b602c879bfc9ba
Nenad Kosanovic <nenad.kosanovic@tx.group>
2022-07-08 10:18:22 +0200

    env selection added.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,11 +2,33 @@ import React from 'react';
 import axios from "axios";
 import ReactJson from "react-json-view";
 
+let localEnv = "https://localhost:8443";
+let a1Env = "https://dmt.a1.unitycms.io/auth";
+let a2Env = "https://dmt.a2.unitycms.io/auth";
+let a3Env = "https://dmt.a3.unitycms.io/auth";
+let a4Env = "https://dmt.a4.unitycms.io/auth";
+let a5Env = "https://dmt.a5.unitycms.io/auth";
+let a6Env = "https://dmt.a6.unitycms.io/auth";
+
+const envOptions = [
+    {label: 'Local', value: localEnv},
+    {label: 'a1', value: a1Env},
+    {label: 'a2', value: a2Env},
+    {label: 'a3', value: a3Env},
+    {label: 'a4', value: a4Env},
+    {label: 'a5', value: a5Env},
+    {label: 'a6', value: a6Env},
+];
+
 export class Login extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {token: "", users: ""}
+        this.state = {
+            token: "",
+            users: "",
+            env: localStorage.getItem('env') || localEnv
+        }
     }
 
     parseJwt(token) {
@@ -25,7 +47,7 @@ export class Login extends React.Component {
             'Authorization': 'bearer ' + token
         };
 
-        axios.get('https://127.0.0.1:8443/resources/users',
+        axios.get(this.state.env + '/resources/users',
             {headers})
             .then(response => this.setState({users: JSON.stringify(response)}))
     }
@@ -36,19 +58,36 @@ export class Login extends React.Component {
             'Authorization': 'Basic Y2QyOmNkMg=='
         };
 
-        axios.post('https://127.0.0.1:8443/oauth/token?client_id=cd2&grant_type=authorization_code&redirect_uri=http://127.0.0.1:8080/login&code=' + code,
+        axios.post(this.state.env + '/oauth/token?client_id=cd2&grant_type=authorization_code&redirect_uri=http://127.0.0.1:8080/login&code=' + code,
             {}, {headers})
             .then(response => this.setState({token: JSON.stringify(response)}))
     }
 
     getCode() {
-        window.location.href = "https://localhost:8443/oauth/authorize?scope=webapp&client_id=cd2&response_type=code&redirect_uri=http://127.0.0.1:8080/login";
+        window.location.href = this.state.env + "/oauth/authorize?scope=webapp&client_id=cd2&response_type=code&redirect_uri=http://127.0.0.1:8080/login";
+    }
+
+    handleEnvChange(event) {
+        localStorage.setItem('env', event.target.value)
+        this.setState({env: event.target.value})
     }
 
     render() {
-
         return (
+
             <div>
+                <div>
+                    <label>
+                        Environment:
+                        <select value={this.state.env} onChange={this.handleEnvChange.bind(this)}>
+                            {envOptions.map((option) => (
+                                <option key={option.label} value={option.value}>{option.label}</option>
+                            ))}
+                        </select>
+                    </label>
+                </div>
+
+
                 <h1>Login code: {this.props.code}</h1>
                 <button
                     onClick={() => this.getCode()}>
